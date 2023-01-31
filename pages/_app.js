e8d9706f7ede5b9d8ad3bb4e69a1eb32d63044d7
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2023-01-31 16:52:34 -0500

    css

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import 'remixicon/fonts/remixicon.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Layout from '../components/Layout/Layout' 
+import {ThemeProvider} from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return( <Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import {useState, useEffect} from 'react'
+import {useTheme} from 'next-themes';
+
 export default function Home() {
   return (
     <h1>Hello World</h1>
